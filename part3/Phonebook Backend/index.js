0396aae16fea2738792bd2fea25204cc636ed4d9
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-07-26 18:04:07 +0600

    3.8*: Phonebook backend step8

--- part3/Phonebook Backend/index.js
+++ part3/Phonebook Backend/index.js
@@ -1,6 +1,26 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(requestLogger)
+app.use(unknownEndpoint)
+morgan.token('body', (req, res) =>
+  req.method === 'POST' ? JSON.stringify(req.body) : ' ',
+);
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
 	{
@@ -77,7 +97,6 @@ app.post('/api/persons', (request, response) => {
             error: 'Person already exists'
         });
     }
-
 	const person = {
 		id: generateId(),
 		name: body.name,
@@ -98,7 +117,10 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on ${PORT}`);
 });
+
