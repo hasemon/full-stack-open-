cba37a8c5e8bb533c41aa32c23adba037d759dc4
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-08-04 18:09:51 +0600

    3.14: Phonebook database, step2

--- part3/Phonebook Backend/index.js
+++ part3/Phonebook Backend/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
 
+const Person = require('./models/person')
 
 app.use(express.static('dist'))
 app.use(cors())
@@ -13,28 +15,8 @@ morgan.token('body', (req, res) =>
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
-
 let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
+
 ];
 
 app.get("/", (request, response) => {
@@ -55,26 +37,20 @@ app.get('/info', (request, response) => {
 })
 
 app.get("/api/persons", (request, response) => {
-	console.log(response)
-	response.json(persons);
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 });
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
-
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	Person.findById(request.params.id).then(person => {
+		if (person) {
+			response.json(person)
+		} else {
+			response.status(404).end()
+		}
+	})
 })
-
-function generateId(){
-	const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0
-	return maxId + 1
-}
-
 
 app.post('/api/persons', (request, response) => {
 	const body = request.body;
@@ -84,26 +60,33 @@ app.post('/api/persons', (request, response) => {
 		});
 	}
 
-	const existingPerson = persons.find( person => person.name === body.name )
-	if (existingPerson) {
-        return response.status(400).json({
-            error: 'Person already exists'
-        });
-    }
-	const person = {
-		id: generateId(),
+	// const existingPerson = persons.find( person => person.name === body.name )
+	// if (existingPerson) {
+    //     return response.status(400).json({
+    //         error: 'Person already exists'
+    //     });
+    // }
+	const person = new Person({
 		name: body.name,
 		number: body.number,
-	};
+	})
 
-	persons = persons.concat(person);
-	response.json(person);
+	person.save().then(savedPerson => {
+		response.json(savedPerson)
+	})
 });
 
 
 
 app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
+	Person.findByIdAndDelete(request.params.id)
+		.then(deletedPerson =>{
+			if (deletedPerson){
+				response.status(204).end()
+			}else{
+				response.status(404).end()
+			}
+		})
 	persons = persons.filter(person => person.id !== id)
 
 	response.status(204).end()
@@ -111,7 +94,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on ${PORT}`);
 });
---
+++ part3/Phonebook Backend/mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+    console.log('Usage: node script.js <password> [<name> <number>]');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url =
+    `mongodb+srv://phonebook:${password}@cluster1.xxqbmbl.mongodb.net/phoneBook?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const PersonSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const Person = mongoose.model('Person', PersonSchema);
+
+if (process.argv.length === 3) {
+    
+    Person.find({}).then(result => {
+        console.log('phonebook:');
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+        process.exit(0);
+    }).catch(error => {
+        console.error('Error fetching phonebook entries:', error);
+        mongoose.connection.close();
+        process.exit(1);
+    });
+} else if (process.argv.length === 5) {
+
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const person = new Person({
+        name: name,
+        number: number,
+    });
+
+    person.save()
+        .then(result => {
+            console.log('Person saved successfully:', result);
+            mongoose.connection.close();
+            process.exit(0);
+        })
+        .catch(error => {
+            console.error('Error saving person:', error);
+            mongoose.connection.close();
+            process.exit(1);
+        });
+} else {
+    console.log('Usage: node script.js <password> [<name> <number>]');
+    mongoose.connection.close();
+    process.exit(1);
+}
