0e321719f6a96c3fd971247c9df1f2cfda479f13
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-09-01 17:57:32 +0600

    Deleted directory for not adding gitingnore file

--- part4/bloglist-backend/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-const {info} = require('./utils/logger')
-const {PORT} = require('./utils/config')
-
-
-app.listen(PORT, () => {
-    info(`Server running on port ${PORT}`)
-})
